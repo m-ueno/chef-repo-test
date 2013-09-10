a0f7f105484c76613ebb92949e6adf2536f64704
UENO, Masaru <hamada@stack6.exp0.net>
2013-09-10 16:36:56 +0900

    deprecated: use ~/.chef/knife.rb

--- solo.rb
+++
@@ -1,9 +0,0 @@
-log_level :info
-
-cookbook_path [File.expand_path("./site-cookbooks"), File.expand_path("./cookbooks")]
-file_cache_path "/tmp/chef-solo"
-role_path File.expand_path "./roles"
-
-http_proxy 'http://proxy.gw.nic.fujitsu.com:8080'
-https_proxy 'http://proxy.gw.nic.fujitsu.com:8080'
-
